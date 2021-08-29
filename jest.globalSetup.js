06786f9e00f763e67d43738b09a667025c3c5d53
Tim Carry <tim@pixelastic.com>
2021-08-29 23:34:47 +0200

    Use GIT_USER_* env vars

--- jest.globalSetup.js
+++ jest.globalSetup.js
@@ -1,13 +1,7 @@
-/**
- * When running the tests as a pre-commit hook, git sets the GIT_AUTHOR_NAME and
- * GIT_AUTHOR_EMAIL environment variables. Those take precendence over any
- * user.name and user.email git config set.
- *
- * This breaks our tests, because with those values are dependent on the values
- * of the global ~/.gitconfig. It is even more confusing because the tests pass
- * when run directly, but fail when run through husky.
- **/
 module.exports = async () => {
-  process.env.GIT_AUTHOR_NAME = 'Gilmore';
-  process.env.GIT_AUTHOR_EMAIL = 'gilmore@gloriousgoods.com';
+  delete process.env.GIT_AUTHOR_NAME;
+  delete process.env.GIT_AUTHOR_EMAIL;
+
+  process.env.GIT_USER_NAME = 'Gilmore';
+  process.env.GIT_USER_EMAIL = 'gilmore@gloriousgoods.com';
 };
